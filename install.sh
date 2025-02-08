c8e252af5237554a7298844615f90952d220ecd4
kajisnakehead <146873827+kajisnakehead@users.noreply.github.com>
2025-02-09 00:16:02 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
+source /root/.bashrc
+gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+gaianet start
