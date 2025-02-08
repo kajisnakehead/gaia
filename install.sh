e6285a6ab4bd8f55e6b6a0e3fd762f7ab748d046
kajisnakehead <146873827+kajisnakehead@users.noreply.github.com>
2025-02-09 00:19:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/
 source /root/.bashrc
 gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 gaianet start
+chmod +x bot.sh
